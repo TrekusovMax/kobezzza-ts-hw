49d387019edf3c4532f6033f5d20afdbc640540f
Trekusov Max <maxtrekusovsite@gmail.com>
2025-04-28 23:56:11 +0300

    add bridge and friend pattern

--- 4/bridge.ts
+++ 4/bridge.ts
@@ -7,3 +7,65 @@
 // * Детализированный рендеринг должен добавлять дополнительные параметры (например, тень или градиент).
 // * Обеспечьте возможность смены реализации без изменения клиентского кода.
 // PS: реализация важна с точки зрения типов. Фактическую реализацию можно не делать.
+interface RenderImplementation {
+  shape: string
+  drawSVG(): void
+  drawCanvas(): void
+}
+interface AbstactFigure {
+  renderer: RenderImplementation
+}
+
+class SimpleRender implements RenderImplementation {
+  shape: string
+  constructor(shape: string) {
+    this.shape = shape
+  }
+  drawCanvas(): void {
+    console.log(`Render ${this.shape} in canvas.`)
+  }
+  drawSVG(): void {
+    console.log(`Render ${this.shape} in SVG.`)
+  }
+}
+
+class DetailRender implements RenderImplementation {
+  shape: string
+  shadow: string
+  constructor(shadow: string, shape: string) {
+    this.shadow = shadow
+    this.shape = shape
+  }
+  drawCanvas(): void {
+    console.log(
+      `Render ${this.shape} in canvas with shadow ${this.shadow}`,
+    )
+  }
+  drawSVG(): void {
+    console.log(`Render ${this.shape} in SVG`)
+  }
+}
+
+class Renderer implements AbstactFigure {
+  renderer: RenderImplementation
+
+  constructor(renderer: RenderImplementation) {
+    this.renderer = renderer
+  }
+
+  drawCanvas(): void {
+    this.renderer.drawCanvas()
+  }
+  drawSVG(): void {
+    this.renderer.drawSVG()
+  }
+}
+
+const circle = new SimpleRender('circle')
+const square = new DetailRender('10', 'square')
+
+const SVGRender = new Renderer(circle)
+const CanvasRender = new Renderer(square)
+
+SVGRender.drawSVG()
+CanvasRender.drawCanvas()
--- 4/friend.ts
+++ 4/friend.ts
@@ -7,3 +7,50 @@
 // * Реализуйте метод проверки, возвращающий, имеет ли сотрудник доступ к определенному ресурсу.
 // * Реализуйте метод изменения уровня доступа с проверкой текущих прав.
 // PS: реализация важна с точки зрения типов. Фактическую реализацию можно не делать.
+
+interface AccessEmployee {
+  id: Employee['id']
+  accessLevel: Employee['accessLevel']
+}
+
+const enum AccessLevel {
+  low = 'low',
+  medium = 'medium',
+  hight = 'hight',
+}
+
+class Employee {
+  constructor(private id: number, private accessLevel: AccessLevel) {}
+
+  get getData(): AccessEmployee {
+    return this as any
+  }
+  set changeLevel(level: AccessLevel) {
+    this.accessLevel = level
+  }
+
+  manager = new AccessManager(this)
+}
+
+class AccessManager {
+  constructor(protected emploee: Employee) {}
+
+  check(level: AccessLevel) {
+    return this.emploee.getData.accessLevel === level
+  }
+  changeAccessLevel(newLevel: AccessLevel) {
+    if (!this.check(newLevel)) {
+      this.emploee.changeLevel = newLevel
+    } else {
+      console.log(`Employee already has '${newLevel}' level!`)
+    }
+  }
+}
+
+const worker = new Employee(1, AccessLevel.low)
+const manager = new AccessManager(worker)
+manager.changeAccessLevel(AccessLevel.low)
+console.log(worker.getData.accessLevel)
+
+manager.changeAccessLevel(AccessLevel.medium)
+console.log(worker.getData.accessLevel)
