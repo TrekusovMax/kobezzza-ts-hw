eb49366be3f4dc1428d6e210ae5129f8a12a6ca8
Maxim Trekusov <trekusovmax@gmail.com>
2025-05-19 16:58:03 +0300

    hw #8 task 2 done

--- 8/2.ts
+++ 8/2.ts
@@ -13,12 +13,33 @@ type AddThisParameter<T, U> = T extends {
   (...args: infer A3): infer R3
   (...args: infer A4): infer R4
 }
-  ? A1 extends Iterator<infer V>
-    ? V
-    : A1 | A2 | A3 | A4
-  : never /* extends { [K in keyof T]: T[K] }
-  ? T // & { this: U }
-  : never */
+  ? {
+      (this: U, ...args: A1): R1
+      (this: U, ...args: A2): R2
+      (this: U, ...args: A3): R3
+      (this: U, ...args: A4): R4
+    }
+  : T extends {
+      (...args: infer A1): infer R1
+      (...args: infer A2): infer R2
+      (...args: infer A3): infer R3
+    }
+  ? {
+      (this: U, ...args: A1): R1
+      (this: U, ...args: A2): R2
+      (this: U, ...args: A3): R3
+    }
+  : T extends {
+      (...args: infer A1): infer R1
+      (...args: infer A2): infer R2
+    }
+  ? {
+      (this: U, ...args: A1): R1
+      (this: U, ...args: A2): R2
+    }
+  : T extends (...args: infer A1) => infer R1
+  ? (this: U, ...args: A1) => R1
+  : never
 
 // { (this: object, a: number, b: number): number; (this: object, a: bigint, b: bigint): bigint }
 type ThisSum = AddThisParameter<typeof sum, object>
